05ea019bbd17f32eff62af3b1ff8cc2b7566b83f
Sabrina Krakau <sabrina.krakau.qbic@gmail.com>
2021-03-24 09:49:44 +0100

    Fix split_fasta.py bug and improve runtime

--- bin/split_fasta.py
+++ bin/split_fasta.py
@@ -19,35 +19,41 @@ min_length_to_retain_contig = int(argv[4])
 # Base name for file output
 out_base = (os.path.splitext(input_file)[0])
 
+# Data structures to separate and store sequences
+df_above_threshold = pd.DataFrame(columns=['id','seq','length'])
+pooled             = []
+remaining          = []
+
 # Read file
 with open(input_file) as f:
     fasta_sequences = SeqIO.parse(f,'fasta')
 
-# make table
-    df = pd.DataFrame(columns=['id','seq','length'])
     for fasta in fasta_sequences:
         name, sequence = fasta.id, str(fasta.seq)
         length = len(sequence)
-        df = df.append({"id":name, "seq":sequence, "length":length}, ignore_index = True)
 
-#sort table by sequence length
-df.sort_values(by=['length'], ascending=False)
+        # store each sequence above threshold together with its length into df
+        if length >= length_threshold:
+            df_above_threshold = df_above_threshold.append({"id":name, "seq":sequence, "length":length}, ignore_index = True)
+        # contigs to retain and pool
+        elif length >= min_length_to_retain_contig:
+            pooled.append(SeqRecord(Seq(sequence, generic_dna), id = name))
+        # remaining sequences
+        else:
+            remaining.append(SeqRecord(Seq(sequence, generic_dna), id = name))
 
-# Save sequences
-remaining = []
-pooled = []
-for index, row in df.iterrows():
-    # each sequence above threshold into one file
-    if row['length'] >= length_threshold and index+1 <= max_sequences:
+# Sort sequences above threshold by length
+df_above_threshold.sort_values(by=['length'], ascending=False, inplace=True)
+df_above_threshold.reset_index(drop=True, inplace=True)
+
+# Write `max_sequences` longest sequences (above threshold) into separate files, add remainder to pooled
+for index, row in df_above_threshold.iterrows():
+    if index+1 <= max_sequences:
         print("write "+out_base+"."+str(index+1)+".fa")
         out = (SeqRecord(Seq(row['seq'], generic_dna), id = row['id']))
         SeqIO.write(out, out_base+"."+str(index+1)+".fa", "fasta")
-    # contigs to retain
-    elif row['length'] >= min_length_to_retain_contig:
-        pooled.append(SeqRecord(Seq(row['seq'], generic_dna), id = row['id']))
-    # remaining sequences
     else:
-        remaining.append(SeqRecord(Seq(row['seq'], generic_dna), id = row['id']))
+        pooled.append(SeqRecord(Seq(row['seq'], generic_dna), id = row['id']))
 
 print("write "+out_base+".pooled.fa")
 SeqIO.write(pooled, out_base+".pooled.fa", "fasta")
