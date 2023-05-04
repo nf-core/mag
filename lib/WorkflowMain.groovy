da67c16d95a9f93fe1edb3c7df3865b6b49f384d
James Fellows Yates <jfy133@gmail.com>
2023-05-04 15:16:25 +0200

    Reduce error function specificitiy

--- lib/WorkflowMain.groovy
+++ lib/WorkflowMain.groovy
@@ -84,7 +84,7 @@ class WorkflowMain {
 
         // Check input has been provided
         if (!params.input) {
-            Nextflow.error("Please provide an input samplesheet to the pipeline e.g. '--input samplesheet.csv'")
+            error("Please provide an input samplesheet to the pipeline e.g. '--input samplesheet.csv'")
         }
     }
     //
