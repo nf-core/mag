89e02bbb3dc9c94ab78afcdb416dd48217f47065
Diego Alvarez S. <dialvarezs@gmail.com>
2026-06-25 10:52:09 -0300

    fix: save gzipped bin files on metabinner to prevent race condition on nfs

--- bin/create_metabinner_bins.py
+++ bin/create_metabinner_bins.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python
 
-## Originally written by Hesham Almessady (@HeshamAlmessady) and Adrian Fritz (@AlphaSquad) in https://github.com/hzi-bifo/mag and released under the MIT license.
+## Originally written by Hesham Almessady (@HeshamAlmessady) and Adrian Fritz (@AlphaSquad)
+# in https://github.com/hzi-bifo/mag and released under the MIT license.
 ## See git repository (https://github.com/nf-core/mag) for full license text.
 
-import sys
+import gzip
+import io
 import os
+import sys
+
 from Bio import SeqIO
+
 
 def main():
     # Argument parsing
     if len(sys.argv) != 6:
-        print("Usage: python create_metabinner_bins.py <binning_file> <fasta_file> <output_path> <prefix> <length_threshold>")
+        print(
+            "Usage: python create_metabinner_bins.py <binning_file> <fasta_file> <output_path> <prefix> <length_threshold>"
+        )
         sys.exit(1)
 
     binning = sys.argv[1]
@@ -19,27 +26,39 @@ def main():
     prefix = sys.argv[4]
     length = int(sys.argv[5])
 
-    # Create output directory if it doesn't exist
+    root = os.path.dirname(os.path.normpath(path)) or "."
     os.makedirs(path, exist_ok=True)
 
-    # Load binning data into a dictionary
-    Metabinner_bins = {}
-    with open(binning, 'r') as b:
+    metabinner_bins = {}
+    with open(binning, "r") as b:
         for line in b:
-            contig, bin = line.strip().split('\t')
-            Metabinner_bins[contig] = bin
+            contig, bin = line.strip().split("\t")
+            metabinner_bins[contig] = bin
 
-    # Process the input fasta file
+    handles = {}
+
+    def get_handle(dest_dir, fname):
+        key = os.path.join(dest_dir, fname)
+        if key not in handles:
+            raw = open(key + ".gz", "wb")
+            gz = gzip.GzipFile(fileobj=raw, mode="wb", mtime=0)
+            handles[key] = (io.TextIOWrapper(gz, encoding="utf-8"), raw)
+        return handles[key][0]
+
     with open(fasta) as handle:
         for record in SeqIO.parse(handle, "fasta"):
             if len(record) < length:
-                f = prefix + ".tooShort.fa"
-            elif record.id not in Metabinner_bins:
-                f = prefix + ".unbinned.fa"
+                out = get_handle(root, prefix + ".tooShort.fa")
+            elif record.id not in metabinner_bins:
+                out = get_handle(root, prefix + ".unbinned.fa")
             else:
-                f = prefix + "." + Metabinner_bins[record.id] + ".fa"
-            with open(os.path.join(path, f), 'a') as out:
-                SeqIO.write(record, out, "fasta")
+                out = get_handle(path, prefix + "." + metabinner_bins[record.id] + ".fa")
+            SeqIO.write(record, out, "fasta")
+
+    for text, raw in handles.values():
+        text.close()
+        raw.close()
+
 
 if __name__ == "__main__":
     main()
